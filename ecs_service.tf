71a6049f5a8919f141229d4b136ae808ac205142
Jonathan <86590925+Jonathan-Jo-me@users.noreply.github.com>
2024-06-07 16:10:27 +0530

    Create ecs_service.tf

---
+++ ecs_service.tf
@@ -0,0 +1,12 @@
+resource "aws_ecs_service" "hello_world" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    assign_public_ip = true
+  }
+}
