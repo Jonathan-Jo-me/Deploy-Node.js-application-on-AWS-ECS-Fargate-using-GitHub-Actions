0de597e6f8d5c3b95a4902cf4ec1a3b78db9c2ae
Jonathan <86590925+Jonathan-Jo-me@users.noreply.github.com>
2024-06-07 16:10:01 +0530

    Create ecs_task_definition.tf

---
+++ ecs_task_definition.tf
@@ -0,0 +1,23 @@
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "hello-world"
+      image = "your-dockerhub-username/hello-world:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
